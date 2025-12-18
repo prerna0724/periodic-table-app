80dbe137fd72663f5fac54ae3d31f6c92e13a5e8
Prerna Lotlikar <lotlikarprerna@gmail.com>
2025-12-18 10:23:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def load_data():
 df = load_data()
 
 # --- TITLE ---
-st.title("Prerna's Periodic Table Explorer 🧪")
+st.title("Prerna's Periodic Table Explorer")
 
 # --- SEARCH + HIGHLIGHT LOGIC (NOW IN MAIN AREA) ---
 # Center the search bar a bit for better looks
